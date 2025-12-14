f86456a4fb726a9eeaec9628e6d721d27230b5ea
Sonakshi Arora <soarora@microsoft.com>
2025-12-14 15:35:04 +0530

    Minor tweaks

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,10 @@ from agent_tools import analyze
 st.title("Talk to your Doc💡")
 st.write("An intelligent document analysis system that uses RAG and AI Agents to analyze **Statement of Work** documents. Built with Azure AI Search, Azure OpenAI, and LangGraph.")
 
+st.write("""Prompt Suggestions 🧠:\n 
+         1. Tell me something about the Contoso project deliverables?\n
+         2. Tell me the risks in Northwind Traders project?""")
+
 mode = st.radio("Select Mode: ", ["Simple RAG", "Contract Agent (For Project Risk Assessment)"])
 
 question = st.text_input("Ask a question about the document:")
